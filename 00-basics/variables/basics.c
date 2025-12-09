8254f281a8f919134ae245ae91d580a283314543
Yassine Baadi <baadiyassine51@gmail.com>
2025-12-10 00:02:41 +0100

    update

---
+++ 00-basics/variables/basics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 25;
+    float height = 1.80f;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
