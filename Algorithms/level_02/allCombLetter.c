84bfbd688b8ee3972b79cfcbbfdaf3eb1f326f08
Yacin-lab <baadiyassine51@gmail.com>
2026-01-07 13:17:50 +0100

    done

--- Algorithms/level_02/allCombLetter.c
+++ Algorithms/level_02/allCombLetter.c
@@ -27,3 +27,39 @@ int main(void)
     allCombLetter();
     return (0);
 }
+
+
+
+/*#include <stdio.h>
+
+void comb3(char one, char tow, char three)
+{
+    printf("%c%c%c\n", one, tow, three);
+    if(three == 'Z')
+        return;
+    comb3(one, tow, three + 1);
+}
+
+void comb2(char one, char tow, char three)
+{
+    comb3(one, tow, three);
+    if(tow == 'Z')
+        return;
+    comb2(one, tow + 1, three);
+}
+
+void comb1(char one, char tow, char three)
+{
+    comb2(one, tow, three);
+    if(one == 'Z')
+        return;
+    comb1(one + 1, tow, three);
+}
+
+int main()
+{
+    char letter = 'A';
+    comb1(letter, letter, letter);
+
+    return 0;
+}*/
