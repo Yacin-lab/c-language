49aafbc95e4b01f5930a77f9c01be53ac38ea8c8
Yassine Baadi <baadiyassine51@gmail.com>
2026-04-06 17:09:31 +0100

    Create swap_using_pointers.c

---
+++ 04-pointers-&-dereference/swap_using_pointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int x = 5;
+    int y = 10;
+
+    swap(&x, &y);
+
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+
+    return 0;
+}
