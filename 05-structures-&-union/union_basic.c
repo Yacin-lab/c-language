9eb07a02ff54bffd8cea472c076afcc41c485c4b
Yassine Baadi <baadiyassine51@gmail.com>
2026-04-23 12:50:11 +0100

    Create union_basic.c

---
+++ 05-structures-&-union/union_basic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+union Data
+{
+    int i;
+    float f;
+    char str[20];
+};
+
+int main()
+{
+    union Data d;
+
+    d.i = 10;
+    printf("i = %d\n", d.i);
+
+    d.f = 5.5;
+    printf("f = %f\n", d.f);
+
+    return 0;
+}
