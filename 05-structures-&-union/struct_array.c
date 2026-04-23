ee6a9d95c83d891e37db439d5facd8e491b295b3
Yassine Baadi <baadiyassine51@gmail.com>
2026-04-23 12:46:45 +0100

    Create struct_array.c

---
+++ 05-structures-&-union/struct_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Student
+{
+    char name[50];
+    int grade;
+};
+
+int main()
+{
+    struct Student students[2] = {
+        {"Ali", 15},
+        {"Sara", 18}
+    };
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%s -> %d\n", students[i].name, students[i].grade);
+    }
+
+    return 0;
+}
