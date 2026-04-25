35f534a0330451a580b014b29594a35293094ff3
Yassine Baadi <baadiyassine51@gmail.com>
2026-04-25 12:16:38 +0100

    Create main_template.c

---
+++ templates/main_template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * Project: [Name of the project]
+ * Purpose: [purpose of the project]
+ * Date: ___/___/____
+ */
+
+int main(int argc, char *argv[]) {
+    // code
+    
+    printf("Ready to code!\n");
+    
+    return 0;
+}
